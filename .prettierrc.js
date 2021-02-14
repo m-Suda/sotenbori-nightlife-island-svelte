f1f064524ca06debbb27c8b22d2a8af78b8ab624
masaks <oicwam.orz1228@gmail.com>
2021-02-14 23:23:16 +0900

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    svelteSortOrder: 'scripts-markup-styles',
+    svelteStrictMode: true,
+};
---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<main></main>
+
+<style lang="scss">
+</style>
